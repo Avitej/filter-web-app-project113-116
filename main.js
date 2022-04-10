82dfded698825884605c1ef611b947992439de38
Avitej <82682098+Avitej@users.noreply.github.com>
2022-04-10 16:48:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(340,340);
+    canvas.position(110,250);
+    canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot() {
+    save('my_filter_image.png');
+}
