5cf4848106c93e967d7ba9c94a8e987d0e6e6a57
Avitej <82682098+Avitej@users.noreply.github.com>
2022-04-17 15:33:41 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,26 @@ function setup() {
     canvas = createCanvas(340,340);
     canvas.position(110,250);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(340, 340);
+    video.hide();
 }
 function draw(){
-
+    image(video, 0, 0, 340, 340);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function take_snapshot() {
     save('my_filter_image.png');
+}
+function modelLoaded() {
+    console.log('posenet is initialized')
+}
+function gotPoses(results) {
+ if (results.length > 0) {
+     console.log(results);
+     console.log("nose x = "+results[0].pose.nose.x);
+     console.log("nose y = "+results[0].pose.nose.y);
+ }   
+
 }
