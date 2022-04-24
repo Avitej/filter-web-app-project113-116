6bca950ae5173b4cdff7812a459bc821a0bdb681
Avitej <82682098+Avitej@users.noreply.github.com>
2022-04-24 16:53:31 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX = 0;
+noseY = 0;
 function preload() {
-
+    clown_nose = loadImage('https://i.postimg.cc/PJbb3pNn/Moustache-PNG-Pic.png');
 }
 function setup() {
     canvas = createCanvas(340,340);
@@ -8,11 +10,12 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(340, 340);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
     image(video, 0, 0, 340, 340);
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+    image(clown_nose, noseX, noseY, 50, 50);
 }
 function take_snapshot() {
     save('my_filter_image.png');
@@ -21,10 +24,12 @@ function modelLoaded() {
     console.log('posenet is initialized')
 }
 function gotPoses(results) {
- if (results.length > 0) {
-     console.log(results);
-     console.log("nose x = "+results[0].pose.nose.x);
-     console.log("nose y = "+results[0].pose.nose.y);
- }   
-
-}
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 25;
+        noseY = results[0].pose.nose.y - 0;
+        console.log("nose x = "+noseX);
+        console.log("nose y = "+noseY);
+    }   
+   
+   }
